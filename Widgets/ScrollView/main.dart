f02d448b45fadbf0f557415c3d2776971e066f47
Francis Jemuel Bergonia <72058376+fjbMultisys@users.noreply.github.com>
2023-03-25 13:25:46 +0800

    Create main.dart

---
+++ Widgets/ScrollView/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+ runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     title: 'Flutter Demo',
+     theme: ThemeData(
+       primaryColor: Colors.white,
+     ),
+     debugShowCheckedModeBanner: false,
+     home: MyHomePage(),
+   );
+ }
+}
+
+class MyHomePage extends StatefulWidget {
+ @override
+ _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+ bool _isAlwaysShown = true;
+
+ bool _showTrackOnHover = false;
+
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(
+       title: Text('Scrollbar Demo' ),
+     ),
+     body: Column(
+       crossAxisAlignment: CrossAxisAlignment.start,
+       children: [
+         Expanded(
+           child: Scrollbar(
+             isAlwaysShown: _isAlwaysShown,
+             showTrackOnHover: _showTrackOnHover,
+             hoverThickness: 30.0,
+             child: ListView.builder(
+               itemCount: 20,
+               itemBuilder: (context, index) => MyItem(index),
+             ),
+           ),
+         ),
+         Divider(height: 1),
+       ],
+     ),
+   );
+ }
+}
+
+class MyItem extends StatelessWidget {
+ final int index;
+
+ const MyItem(this.index);
+
+ @override
+ Widget build(BuildContext context) {
+   final color = Colors.primaries[index % Colors.primaries.length];
+   final hexRgb = color.shade500.toString().substring(10, 16).toUpperCase();
+   return ListTile(
+     contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+     leading: AspectRatio(
+         aspectRatio: 1,
+         child: Container(
+           color: color,
+         )),
+     title: Text('Material Color #${index + 1}'),
+     subtitle: Text('#$hexRgb'),
+   );
+ }
+}
